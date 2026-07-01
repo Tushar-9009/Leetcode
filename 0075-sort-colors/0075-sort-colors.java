7d4bbc1b7bc87854e44389b51f42447f20860a0f
Tushar-9009 <121216602+Tushar-9009@users.noreply.github.com>
2026-07-01 08:40:40 +0530

    Time: 1 ms (22.43%), Space: 43 MB (98.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,24 +1,23 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int count_z = 0;
-        int count_o = 0;
-        int count_t = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0)
-                count_z++;
-            else if (nums[i] == 1)
-                count_o++;
-            else
-                count_t++;
+        int n=nums.length;
+        ArrayList<Integer> list1= new ArrayList<>();
+        ArrayList<Integer> list2= new ArrayList<>();
+        ArrayList<Integer> list3= new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                list1.add(nums[i]);
+            }else if(nums[i]==1){
+                list2.add(nums[i]);
+            }else{
+                list3.add(nums[i]);
+            }
         }
-        for (int i = 0; i < n; i++) {
-            if (i < count_z)
-                nums[i] = 0;
-            else if (i < count_o + count_z)
-                nums[i] = 1;
-            else
-                nums[i] = 2;
+        list1.addAll(list2);
+        list1.addAll(list3);
+        for(int i=0;i<n;i++){
+            nums[i]=list1.get(i);
         }
+
     }
 }

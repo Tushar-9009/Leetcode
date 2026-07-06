cfa302f0672dc57da115f98ca8eeffd512c49a9f
Tushar-9009 <121216602+Tushar-9009@users.noreply.github.com>
2026-07-06 10:31:27 +0530

    Time: 3 ms (60.61%), Space: 44.7 MB (48.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isPalindrome(String s) {
         int n=s.length();
-        String res="";
+        StringBuilder res=new StringBuilder();
         for(int i=0;i<n;i++){
             char ch=s.charAt(i);
             if(ch>=65 && ch<=90){
                 ch=(char)(ch+32);
-                res+=ch;
+                res.append(ch);
             }
             else if(ch>=97 && ch<=122)
-            res+=ch;
+            res.append(ch);
             else if(ch>='0' && ch<='9') 
-            res+=ch;
+            res.append(ch);
         }
         int m=res.length();
         for(int i=0;i<m/2;i++){
